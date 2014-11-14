bfbd4c6382731d48e006966d12ff633fb5ebebf9
James Lamine <jgl5@students.calvin.edu>
2014-11-14 13:45:50 -0400

    Add util class for lab 11 testing

---
+++ src/com/lamine/lab1/Util.java
@@ -0,0 +1,11 @@
+package com.lamine.lab1;
+
+public class Util {
+	public static int addNumbers(int a, int b) {
+		return a + b;
+	}
+	
+	public static int increment(int a) {
+		return a + 1;
+	}
+}
